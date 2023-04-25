23b8f1228cbfa243a40a67722c345326861dd413
juanbrusasca <jbrusasca@lasalleflorida.edu.ar>
2023-04-25 19:05:20 -0300

    Tienda_Online_1

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function escribir(){
+    document.getElementById('aca').textContent = 'hola';
+    
+}
+function agregarCarrito(){
+    
+    let text = document.getElementsByClassName("prod").textContent; 
+    console.log(text);
+}
